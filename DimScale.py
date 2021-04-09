7de830b9272031f491a0147bbcc2a6993c6fcfef
Ricardo Roman-Brenes <ricardo@bilkent.edu.tr>
2021-04-09 15:49:50 +0300

    viz for 2d and 3d pcas. convex hull using sklear

--- DimScale.py
+++ DimScale.py
@@ -6,6 +6,10 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 
+import plotly.graph_objects as go
+import itertools, math, numpy as np
+
+from scipy.spatial import ConvexHull
 
 def PCA_2(data):
 
@@ -19,7 +23,7 @@ def PCA_2(data):
                        y=1,
                        color=data[data.columns[-1]],
                        title="Reduction to 2 PCA")
-    fig_1.show()
+    #fig_1.show()
 
     total_var = pca.explained_variance_ratio_.sum() * 100
     print("Variance explained with 2 PCAs: ", total_var)
@@ -34,12 +38,13 @@ def PCA_2(data):
             "y": "Explained Variance"
         },
         title="Variance explained with 2 PCAs: " + str(total_var))
-    fig_2.show()
+    #fig_2.show()
 
     return {
         "fig_pca": fig_1,
         "fig_exp_var": fig_2,
-        "total_variance": total_var
+        "total_variance": total_var,
+        "pca": components
     }
 
 
@@ -55,7 +60,7 @@ def PCA_3(data):
                           z=2,
                           color=data[data.columns[-1]],
                           title="Reduction to 3 PCA")
-    fig_1.show()
+    #fig_1.show()
 
     total_var = pca.explained_variance_ratio_.sum() * 100
     print("Variance explained with 3 PCAs: ", total_var)
@@ -70,10 +75,35 @@ def PCA_3(data):
             "y": "Explained Variance"
         },
         title="Variance explained with 3 PCAs: " + str(total_var))
-    fig_2.show()
+    #fig_2.show()
 
     return {
         "fig_pca": fig_1,
         "fig_exp_var": fig_2,
-        "total_variance": total_var
+        "total_variance": total_var,
+        "pca": components
     }
+
+
+
+
+def visualize_3d(pca):
+    x = np.array(pca)
+    ch = x[ConvexHull(x).vertices]
+    print('poınts of data', len(x))
+    print('points in hulllen', len(ch))
+
+
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Mesh3d(x=ch[:, 0], 
+                            y=ch[:, 1], 
+                            z=ch[:, 2], 
+                            color="blue", opacity=.6, alphahull=0))
+    fig.add_trace(go.Scatter3d(x=x[:, 0], 
+                            y=x[:, 1], 
+                            z=x[:, 2], 
+                            mode='markers'))
+    fig.show()
+    return
--- main.py
+++ main.py
@@ -6,29 +6,56 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from DimScale import *
+from dna2vec import *
+from pprint import pprint
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
 
 
 # data should be presented in a Pandas DataFrame with all numerical 
 # attributes but the last one (category/class)
+reads = []
+l = 200
 
-# lists
-A =     [  1,    1,    0,    0,    1,    0,    1,    1,    0,    0]
-C =     [  0,    0,    1,    0,    1,    1,    1,    1,    1,    0]
-G =     [  1,    0,    1,    0,    1,    0,    1,    0,    1,    0]
-T =     [  1,    1,    1,    0,    1,    1,    1,    0,    0,    0]
-CLS =   ["C1", "C1", "C2", "C3", "C3", "C4", "C5", "C6", "C7", "C7"]
-  
-# dictionary of lists 
-dict = {'Adenine': A, 'Cytocine': C, 'Guanine': G, 'Tymine': T, 'class': CLS} 
-
-df = pd.DataFrame(dict)
-
-print(df)
-
-########################### PCAs ###########################
+for i in range(l):
+    reads.append(dna2vec(random_dna_seq(l)))
 
 
-r = PCA_2(df)
-print(r)
+
+df = pd.DataFrame(reads)
+pprint(df)
+
 r = PCA_3(df)
-print(r)
+
+#r['fig_pca'].show()
+vec_save(r['pca'], 'out.vec')
+# pprint(r)
+
+
+
+visualize_3d(r['pca'])
+
+
+
+'''
+pprint(r["pca"])
+
+points = r["pca"]
+hull = ConvexHull(points)
+#Plot it:
+
+
+plt.plot(points[:,0], points[:,1], 'o')
+for simplex in hull.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+#We could also have directly used the vertices of the hull, which for 2-D are guaranteed to be in counterclockwise order:
+
+
+plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
+plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+plt.show()
+'''
